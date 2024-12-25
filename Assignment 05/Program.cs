b445c102561fd811e0bf37c2c4fe1b346be34ed1
Mohamed Atef <Mohamed Atef>
2024-12-25 20:47:18 +0200

    Nullable Value Type

--- Assignment 05/Program.cs
+++ Assignment 05/Program.cs
@@ -33,6 +33,45 @@
             //// unsafe casting 
             #endregion
 
+            #region Nullable Value Type
+            // Nullable Types
+
+            // Value type
+            // Reference type
+
+
+            //int x = 5;
+            //x = null; // error not valid
+
+            //int? age = 20; // Nullable type allow null
+            //age = null; 
+
+            //double? salary = 5000.0;
+
+            //salary = null;
+
+            //int? x = 5;
+
+            //int? y = x;
+            //// implicit casting
+            //// safe casting
+
+            //int? x =5;
+            //int y =(int) x;
+            //// explicit casting
+            //// unsafe casting
+
+            //if (x != null)
+            //    y = (int)x;
+            //else
+            //    y = 0; 
+            #endregion
+
+
+
+
+
+
 
         }
     }
