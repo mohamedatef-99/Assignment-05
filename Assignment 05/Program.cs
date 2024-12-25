27a6e98ef5cfcd3a2cdf0184efd60ab94498f6f9
Mohamed Atef <Mohamed Atef>
2024-12-25 20:40:23 +0200

    Boxing and Unboxing

--- Assignment 05/Program.cs
+++ Assignment 05/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Boxing and Unboxing
+            //// Boxing and Unboxing
+
+            //// Boxing: casting from valueType to referenceType
+            //// Unboxing: casting from referenceType to valueType
+
+            //object o1;
+
+            //// o1 ==> can refere to instance of Object or an instance of any type
+
+            //o1 = 10; // cast from int to object
+            //o1 = "Hello"; // cast from string to object
+
+            //int x = 5;
+            //object o1 = x;
+
+            //// implicit casing
+            //// safe casting
+            //// boxing
+            //// parent = child => child is parent
+
+            //object o1 = 3;
+            //o1 = "Hello";
+
+            //// unboxing
+            //// explicit casting
+            //// unsafe casting 
+            #endregion
+
+
         }
     }
 }
